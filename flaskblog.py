6f851d54534d203e93fc746dde6fb8b55f8c67a3
Mutia <dmutia@deloitte.co.ke>
2021-01-12 05:35:44 +0300

    feat: Create a form template for registration and login

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '902ac67844feec00bd5b49aeeb983cf8'
 
 posts = [
     {
@@ -25,6 +28,19 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
